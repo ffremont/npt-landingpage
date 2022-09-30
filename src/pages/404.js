594a24777e609347d02f613a11f5a308e0350706
Tokenizers <romain.barraud1@gmail.com>
2022-09-30 14:05:30 +0200

    initial commit

---
+++ src/pages/404.js
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+const NotFoundPage = () => {
+  return <main>404</main>;
+};
+
+export default NotFoundPage;
+
+export const Head = () => <title>Not found</title>;
